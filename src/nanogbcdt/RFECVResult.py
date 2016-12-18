7e6629b9a9e9732401535d89116df1c3599c55aa
invisilun <sami@galal.li>
2016-12-18 09:56:30 +0100

    Playing with the idea of printing a class

--- src/nanogbcdt/RFECVResult.py
+++ src/nanogbcdt/RFECVResult.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+from __future__ import print_function
 import pandas as pd
 
 
@@ -11,3 +13,10 @@ class RFECVResult:
         self.feature_importances_ = pd.DataFrame()
         self.grid_scores_ = pd.DataFrame()
         self.holdout_predictions_ = []
+
+#    def __str__(self):
+#        return print("{} [name_list_={}, optimum_lengths_={}, class_scores_f1_={}, class_scores_r2_={}, "
+#                     "class_scores_mae_={}, feature_importances_={}, grid_scores_={}, holdout_predictions_={}]".format(
+#                         self.__class__.__name__, self.name_list_, self.optimum_lengths_, self.class_scores_f1_,
+#                         self.class_scores_r2_, self.class_scores_r2_, self.feature_importances_, self.grid_scores_,
+#                         self.holdout_predictions_))
